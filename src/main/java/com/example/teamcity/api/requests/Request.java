05d1eed6577a1bb824c618e32a5e2a52625c1a41
Viktoriia.Bespiatova <Viktoriia.Bespiatova@interone.de>
2024-10-05 17:42:43 +0200

    Lesson_Requests_Classification: Add Checked and Unchecked classes & update Request.java

--- src/main/java/com/example/teamcity/api/requests/Request.java
+++ src/main/java/com/example/teamcity/api/requests/Request.java
@@ -8,8 +8,8 @@ public class Request {
      * Request - это класс, описывающий меняющиеся параметры запроса, такие как:
      *  спецификация, эндпоинт (relative URL, model)
      */
-    private final RequestSpecification spec;
-    private final Endpoint endpoint;
+    protected final RequestSpecification spec;
+    protected final Endpoint endpoint;
 
     public Request(RequestSpecification spec, Endpoint endpoint) {
         this.spec = spec;
