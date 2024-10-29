6b46d09b01058c557e40f0085162c694d31b8804
Viktoriia.Bespiatova <Viktoriia.Bespiatova@interone.de>
2024-10-29 08:27:04 +0100

    Ui-test: Code for the first 3 lessons

---
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -0,0 +1,29 @@
+package com.example.teamcity.ui;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.example.teamcity.api.BaseTest;
+import com.example.teamcity.api.config.Config;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.Map;
+
+public class BaseUiTest extends BaseTest {
+    @BeforeSuite(alwaysRun = true)
+    public void setupUiTest() {
+        Configuration.browser = Config.getProperty("browser");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        // НЕ ПИШИТЕ UI ТЕСТЫ С ЛОКАЛЬНЫМ БРАУЗЕРОМ
+        // А ПОТОМ ЗАПУСКАЕТЕ НА REMOTE BROWSER
+        Configuration.remote = Config.getProperty("remote");
+        Configuration.browserSize = Config.getProperty("browserSize");
+
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", true, "enableLog", true));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closeWebDriver() {
+        Selenide.closeWebDriver();
+    }
+}
---
+++ src/test/java/com/example/teamcity/ui/CreateProjectTest.java
@@ -0,0 +1,52 @@
+package com.example.teamcity.ui;
+
+import com.example.teamcity.api.enums.Endpoint;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Test(groups = {"Regression"})
+public class CreateProjectTest extends BaseUiTest {
+    @Test(description = "User should be able to create project", groups = {"Positive"})
+    public void userCreatesProject() {
+        // подготовка окружения
+        step("Login as user");
+
+        // взаимодействие с UI
+        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
+        step("Send all project parameters (repository URL)");
+        step("Click `Proceed`");
+        step("Fix Project Name and Build Type name values");
+        step("Click `Proceed`");
+
+        // проверка состояния API
+        // (корректность отправки данных с UI на API)
+        step("Check that all entities (project, build type) was successfully created with correct data on API level");
+
+        // проверка состояния UI
+        // (корректность считывания данных и отображение данных на UI)
+        step("Check that project is visible on Projects Page (http://localhost:8111/favorite/projects)");
+    }
+
+    @Test(description = "User should not be able to craete project without name", groups = {"Negative"})
+    public void userCreatesProjectWithoutName() {
+        // подготовка окружения
+        step("Login as user");
+        step("Check number of projects");
+
+        // взаимодействие с UI
+        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
+        step("Send all project parameters (repository URL)");
+        step("Click `Proceed`");
+        step("Set Project Name");
+        step("Click `Proceed`");
+
+        // проверка состояния API
+        // (корректность отправки данных с UI на API)
+        step("Check that number of projects did not change");
+
+        // проверка состояния UI
+        // (корректность считывания данных и отображение данных на UI)
+        step("Check that error appears `Project name must not be empty`");
+    }
+}
